964ae08892dc769106851b14f075a70d753a0046
yhk1105 <yanghk1105@gmail.com>
2025-12-01 23:15:46 +0800

    chore: Move dotenv loading to config.py for better environment variable management

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask
 from .config import Config
 from .extensions import db
-from dotenv import load_dotenv
 
-load_dotenv()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
--- backend/app/config.py
+++ backend/app/config.py
@@ -1,6 +1,7 @@
 import os
+from dotenv import load_dotenv
 
-
+load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
